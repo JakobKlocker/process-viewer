5b1363a1fe124b547dda840c6c44a23f627d0ac6
Jakob K <jakobklocker@hotmail.com>
2025-03-10 22:34:20 +0100

    cleanup, handle keys in own function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 mod process;
-use process::ProcessInfo;
-use crossterm::event::KeyModifiers;
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEventKind},
+    event::{self, KeyCode, KeyEventKind},
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
+use process::ProcessInfo;
 use ratatui::{
     Terminal,
     backend::CrosstermBackend,
@@ -14,7 +13,7 @@ use ratatui::{
 };
 use std::io::{self, stdout};
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     // Setup Tui
     enable_raw_mode().unwrap();
     let mut stdout = stdout();
@@ -28,7 +27,6 @@ fn main() -> io::Result<()>{
         .iter()
         .map(|p| p.to_string())
         .collect();
-    
 
     let mut selected_proc: usize = 0;
     let mut state = ListState::default();
@@ -40,13 +38,13 @@ fn main() -> io::Result<()>{
                     .iter()
                     .enumerate()
                     .map(|(i, item)| {
-                        let style = if i == selected_proc{
+                        let style = if i == selected_proc {
                             Style::new().fg(ratatui::style::Color::Yellow)
                         } else {
                             Style::new()
-                            };
-                     ListItem::new(item.clone()).style(style)
-                         })
+                        };
+                        ListItem::new(item.clone()).style(style)
+                    })
                     .collect();
 
                 let list = List::new(items)
@@ -56,29 +54,38 @@ fn main() -> io::Result<()>{
                 frame.render_stateful_widget(list, frame.area(), &mut state);
             })
             .unwrap();
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press {
-                if key.code == KeyCode::Char('q') {
-                    cleanup();
-                    return Ok(());
-                }
-            if key.code == KeyCode::Down && selected_proc < proc.len() - 1{
-                selected_proc += 1;
-                state.select(Some(selected_proc));
-            }
-            
-            if key.code == KeyCode::Up && selected_proc > 0{
-                selected_proc -= 1;
-                state.select(Some(selected_proc));
-            }
+            if let Err(()) = handle_key(&mut selected_proc, &mut state, &proc.len()){
+                cleanup();
+                return Ok(());
             }
         }
-    }
-
-    cleanup();
 }
 
-fn cleanup(){
+fn cleanup() {
     disable_raw_mode().unwrap();
     execute!(io::stdout(), LeaveAlternateScreen).unwrap();
+}
+
+fn handle_key(selected_proc: &mut usize, state: &mut ListState, proc_len: &usize) -> Result<(), ()> {
+    if let event::Event::Key(key) = event::read().map_err(|_| ())? {
+        if key.kind == KeyEventKind::Press {
+            if key.code == KeyCode::Char('q') {
+                return Err(());
+            }
+            if (key.code == KeyCode::Down || key.code == KeyCode::Char('k'))
+                && *selected_proc < *proc_len - 1
+            {
+                *selected_proc += 1;
+                state.select(Some(*selected_proc));
+            }
+
+            if (key.code == KeyCode::Up || key.code == KeyCode::Char('j')) && *selected_proc > 0 {
+                *selected_proc -= 1;
+                state.select(Some(*selected_proc));
+            }
+        }
+        return Ok(());
+    } else {
+        return Err(());
+    }
 }
