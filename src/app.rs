7ce6dd39ab4585c6858a09a5b167868c276380f2
Jakob K <jakobklocker@hotmail.com>
2025-04-20 21:23:48 +0200

    simple app tests

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use crate::process::{ProcessInfo, Processes};
 use std::cmp::Reverse;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub enum AppState {
     Normal,      // Default, navigating
     Filtering,   // Filterting Processes
@@ -54,3 +54,75 @@ impl App {
         //self.selected_proc = 0;    since reloading happening all the time, this might crash if its high and on reload proceses are less than before...
     }
 }
+
+#[test]
+fn test_app_initialization() {
+    let app = App::new();
+    assert_eq!(app.processes, app.all_processes);
+    assert_eq!(app.selected_proc, 0);
+    assert_eq!(app.filter_string, "");
+    assert_eq!(app.state, AppState::Normal);
+}
+
+#[test]
+fn test_sort_ascending_by_pid() {
+    let mut app = App {
+        all_processes: vec![],
+        processes: vec![
+            ProcessInfo::new(3, "c".into(), 0, 0),
+            ProcessInfo::new(1, "a".into(), 0, 0),
+            ProcessInfo::new(2, "b".into(), 0, 0),
+        ],
+        selected_proc: 0,
+        filter_string: String::new(),
+        state: AppState::Normal,
+    };
+
+    app.sort_ascending();
+
+    let pids: Vec<u32> = app.processes.iter().map(|p| p.pid).collect();
+    assert_eq!(pids, vec![1, 2, 3]);
+}
+
+#[test]
+fn test_sort_descending_by_pid() {
+    let mut app = App {
+        all_processes: vec![],
+        processes: vec![
+            ProcessInfo::new(1, "a".into(), 0, 0),
+            ProcessInfo::new(3, "c".into(), 0, 0),
+            ProcessInfo::new(2, "b".into(), 0, 0),
+        ],
+        selected_proc: 0,
+        filter_string: String::new(),
+        state: AppState::Normal,
+    };
+
+    app.sort_descending();
+
+    let pids: Vec<u32> = app.processes.iter().map(|p| p.pid).collect();
+    assert_eq!(pids, vec![3, 2, 1]);
+}
+
+#[test]
+fn test_apply_filter_matches_lowercase() {
+    let all = vec![
+        ProcessInfo::new(1, "firefox".into(), 0, 0),
+        ProcessInfo::new(2, "sshd".into(), 0, 0),
+        ProcessInfo::new(3, "gnome-shell".into(), 0, 0),
+    ];
+
+    let mut app = App {
+        all_processes: all.clone(),
+        processes: all.clone(),
+        selected_proc: 0,
+        filter_string: "fire".into(),
+        state: AppState::Filtering,
+    };
+
+    app.apply_filter();
+
+    assert_eq!(app.processes.len(), 1);
+    assert_eq!(app.processes[0].name, "firefox");
+}
+
